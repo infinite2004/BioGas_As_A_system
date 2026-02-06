8781f25acec5ec895f984db64ffe4243bbd7111b
infinite2004 <qureshi.abdul.227@gmail.com>
2026-02-05 19:37:54 -0500

    more files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BioGas_As_A_system/',   // <-- repo name, exact casing
   plugins: [react()],
 })
